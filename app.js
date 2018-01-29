3bd8bcd10e85b3aecfd7ced51d098d475500bf05
pavlvs <pavlvsx@gmail.com>
2018-01-29 17:26:25 +0100

    add validation and categorise BMI result

--- app.js
+++ app.js
@@ -9,7 +9,25 @@ calcButton.addEventListener('click', function () {
     // check if we have clean inout i.e numbers
     if (!isNaN(height) || !isNaN(weight)) {
         let BMI = calcBMI(height, weight).toFixed(2);
-        document.getElementById('output').textContent = 'Your Body Mass Index (BMI) is: ' + BMI;
+        let message = '';
+        switch (true) {
+        case BMI < 18.5:
+            message = 'You are underweight';
+            break;
+        case BMI > 18.5 && BMI < 25:
+            message = 'Your weight is normal';
+            break;
+        case BMI > 25 && BMI < 30:
+            message = 'You are overweight';
+            break;
+        case BMI > 30:
+            message = 'You are obese';
+            break;
+        default:
+            message = '';
+            break;
+        }
+        document.getElementById('output').innerHTML = 'Your Body Mass Index (BMI) is: ' + BMI + '. <br>' + message + '.';
     } else {
         // we really need numbers
         alert('Please enter height and weight as numbers');
